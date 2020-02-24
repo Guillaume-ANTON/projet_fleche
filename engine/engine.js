e6f288a1bf80cb950f707def39923520823a6412
guillaume.anton@ynov.com <guillaume.anton@ynov.com>
2020-02-24 12:24:49 +0100

    add project

--- engine/engine.js
+++ engine/engine.js
@@ -1,8 +1,13 @@
 var inquirer = require('inquirer');
+var Players = require ('../models/Players');
+
 
 console.log('Engine lancé !!');
 
 var nbPlayers = null;
+var Mode = null;
+var Players = null;
+// var test = ['guigui', 'toto', 'ouioui']
 
 function checkPlayersInStart (nombre) {
   if(nombre > 1 && nombre < 5) {
@@ -10,6 +15,11 @@ function checkPlayersInStart (nombre) {
   } else {
     return "Entrer un nombre compris entre 2 et 4";
   }
+}
+
+function randPlayers (Players){
+  const randomElement = Players[Math.floor(Math.random() * Players.length)];
+  console.log(randomElement)
 }
 
 var questionJoueurs = [
@@ -22,25 +32,50 @@ var questionJoueurs = [
   }
 ]
 
-var questionNomJoeurs = []
+var questionNomJoueurs = []
+
+var choiceMode = [
+  {
+    type: 'list',
+    name: 'modeGame',
+    message: 'Choisisez le mode de jeux',
+    choices: ['Tour du monde', '301', 'Cricket']
+
+  }
+]
 
 inquirer.prompt(questionJoueurs).then(answers => {
   nbPlayers = answers.nbPlayers;
   for (var i = 0; i < nbPlayers; i++) {
-    questionNomJoeurs.push({
+    questionNomJoueurs.push({
       type: 'input',
       name: 'namePlayers'+(i+1),
       message: 'Entrer le nom du joueur'+(i+1)
     })
   }
-  inquirer.prompt(questionNomJoeurs).then(answers => {
-    console.log(answers)
+  inquirer.prompt(questionNomJoueurs).then(answers => {
     for (var namePlayers in answers) {
-    }
+      let Players = answers.namePlayers
+      let players = new Players(answers[namePlayers]);
+      console.log(players)
+      console.log(Players)
+  }
+    inquirer.prompt(choiceMode).then(answers => {
+      Mode = answers.modeGame
+      console.log(Mode)
+      randPlayers();
+      if(Mode === 'Tour du monde'){
+        console.log(`Vous avez choisi le ${Mode}`)
+      } else if(Mode === "301"){
+        console.log(`Vous avez choisi le ${Mode}`)
+      } else {
+        console.log(`Vous avez choisi le ${Mode}`)
+      }
+    })
   })
 });
 
 
 
-
-
+// Sélection du joueur qui commence en random 
+// Function shoot
