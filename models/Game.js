414dad7985749b39873bca6e8818e4fb9463813d
guillaume.anton@ynov.com <guillaume.anton@ynov.com>
2020-02-25 22:44:09 +0100

    forget s

--- models/Game.js
+++ models/Game.js
@@ -7,7 +7,7 @@ module.exports = {
     return db.run("SELECT * FROM Game")
   },
   newGame(){
-    return db.run("INSERT INTO Games (mode, name, status, cratedAt) VALUES(?, ?, ?, date('now'))", [body.mode, body.name, body.status])
+    return db.run("INSERT INTO Game (mode, name, status, cratedAt) VALUES(?, ?, ?, date('now'))", [body.mode, body.name, body.status])
   },
   getGameId(){
     return db.run("SELECT FROM Game WHERE id = ?", [body.id])
