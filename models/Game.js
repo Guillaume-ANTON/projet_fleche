18b1f025335e35edcd60195e227e8626311a4cc6
guillaume.anton@ynov.com <guillaume.anton@ynov.com>
2020-02-25 22:43:26 +0100

    forget ?

--- models/Game.js
+++ models/Game.js
@@ -2,12 +2,12 @@ const db = require('sqlite')
 
 // Function route ---> db
 module.exports = {
-  
+
   getGame(){
     return db.run("SELECT * FROM Game")
   },
   newGame(){
-    return db.run("INSERT INTO Games (mode, name, status, cratedAt) VALUES(?, ?, date('now'))", [body.mode, body.name, body.status])
+    return db.run("INSERT INTO Games (mode, name, status, cratedAt) VALUES(?, ?, ?, date('now'))", [body.mode, body.name, body.status])
   },
   getGameId(){
     return db.run("SELECT FROM Game WHERE id = ?", [body.id])
